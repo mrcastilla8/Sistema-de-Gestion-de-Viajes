f35e12b0823b6d63194440c7ee2e8cb3316333a3
MarcosBasualdo25 <balemarcos25@gmail.com>
2024-12-01 20:31:37 -0500

    2do push mejora interfaz rutas

--- src/Vista/IguRuta.java
+++ src/Vista/IguRuta.java
@@ -30,6 +30,7 @@ public class IguRuta extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel4 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
@@ -42,18 +43,30 @@ public class IguRuta extends javax.swing.JFrame {
         jLabel8 = new javax.swing.JLabel();
         txtfHoraLlegada = new javax.swing.JTextField();
         txtfHoraSalida = new javax.swing.JTextField();
-        jPanel2 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel4 = new javax.swing.JLabel();
         btnAgregar = new javax.swing.JButton();
         btnModificar = new javax.swing.JButton();
         btnEliminar = new javax.swing.JButton();
         btnNuevo = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
-        jPanel3 = new javax.swing.JPanel();
+        jLabel6 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         TablaRutas = new javax.swing.JTable();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jPanel4 = new javax.swing.JPanel();
+        jLabel13 = new javax.swing.JLabel();
+
+        jPanel4.setBackground(new java.awt.Color(46, 138, 153));
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1370, Short.MAX_VALUE)
+        );
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 780, Short.MAX_VALUE)
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(0, 204, 204));
@@ -67,7 +80,7 @@ public class IguRuta extends javax.swing.JFrame {
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(585, 23, -1, -1));
 
         jPanel1.setBackground(new java.awt.Color(31, 110, 140));
-        jPanel1.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(255, 255, 255)));
+        jPanel1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 3, true));
 
         jLabel2.setFont(new java.awt.Font("SF Pro Display", 1, 17)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
@@ -82,6 +95,9 @@ public class IguRuta extends javax.swing.JFrame {
         jLabel5.setText("IdRuta");
 
         txtfLugarInicio.setBackground(new java.awt.Color(137, 167, 161));
+        txtfLugarInicio.setFont(new java.awt.Font("SF Pro Display", 0, 17)); // NOI18N
+        txtfLugarInicio.setForeground(new java.awt.Color(255, 255, 255));
+        txtfLugarInicio.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 2, true));
         txtfLugarInicio.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtfLugarInicioActionPerformed(evt);
@@ -89,9 +105,15 @@ public class IguRuta extends javax.swing.JFrame {
         });
 
         txtfLugarDestino.setBackground(new java.awt.Color(137, 167, 161));
+        txtfLugarDestino.setFont(new java.awt.Font("SF Pro Display", 0, 17)); // NOI18N
+        txtfLugarDestino.setForeground(new java.awt.Color(255, 255, 255));
+        txtfLugarDestino.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 2, true));
 
         txtfIdRuta.setEditable(false);
         txtfIdRuta.setBackground(new java.awt.Color(137, 167, 161));
+        txtfIdRuta.setFont(new java.awt.Font("SF Pro Display", 0, 17)); // NOI18N
+        txtfIdRuta.setForeground(new java.awt.Color(255, 255, 255));
+        txtfIdRuta.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 2, true));
 
         jLabel7.setFont(new java.awt.Font("SF Pro Display", 1, 17)); // NOI18N
         jLabel7.setForeground(new java.awt.Color(255, 255, 255));
@@ -102,8 +124,14 @@ public class IguRuta extends javax.swing.JFrame {
         jLabel8.setText("Hora de llegada");
 
         txtfHoraLlegada.setBackground(new java.awt.Color(137, 167, 161));
+        txtfHoraLlegada.setFont(new java.awt.Font("SF Pro Display", 0, 17)); // NOI18N
+        txtfHoraLlegada.setForeground(new java.awt.Color(255, 255, 255));
+        txtfHoraLlegada.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 2, true));
 
         txtfHoraSalida.setBackground(new java.awt.Color(137, 167, 161));
+        txtfHoraSalida.setFont(new java.awt.Font("SF Pro Display", 0, 17)); // NOI18N
+        txtfHoraSalida.setForeground(new java.awt.Color(255, 255, 255));
+        txtfHoraSalida.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 255), 2, true));
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -138,9 +166,9 @@ public class IguRuta extends javax.swing.JFrame {
                     .addComponent(txtfLugarDestino, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3))
                 .addGap(21, 21, 21)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel5)
-                    .addComponent(txtfIdRuta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtfIdRuta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel5))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel7)
@@ -154,97 +182,84 @@ public class IguRuta extends javax.swing.JFrame {
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 110, -1, -1));
 
-        jPanel2.setBackground(new java.awt.Color(46, 138, 153));
+        jPanel3.setBackground(new java.awt.Color(46, 138, 153));
+        jPanel3.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        getContentPane().add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(-380, 450, 990, -1));
+
+        jLabel4.setFont(new java.awt.Font("SF Pro Display", 1, 22)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("DATOS");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 80, -1, -1));
 
         btnAgregar.setBackground(new java.awt.Color(14, 41, 84));
-        btnAgregar.setFont(new java.awt.Font("SF Pro Display", 1, 14)); // NOI18N
+        btnAgregar.setFont(new java.awt.Font("SF Pro Display", 1, 22)); // NOI18N
         btnAgregar.setForeground(new java.awt.Color(255, 255, 255));
         btnAgregar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/crear.png"))); // NOI18N
         btnAgregar.setText("Crear");
+        btnAgregar.setPreferredSize(new java.awt.Dimension(76, 27));
         btnAgregar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnAgregarActionPerformed(evt);
             }
         });
+        getContentPane().add(btnAgregar, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 370, 130, 40));
 
         btnModificar.setBackground(new java.awt.Color(14, 41, 84));
-        btnModificar.setFont(new java.awt.Font("SF Pro Display", 1, 14)); // NOI18N
+        btnModificar.setFont(new java.awt.Font("SF Pro Display", 1, 22)); // NOI18N
         btnModificar.setForeground(new java.awt.Color(255, 255, 255));
         btnModificar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/modificar.png"))); // NOI18N
         btnModificar.setText("Modificar");
+        btnModificar.setPreferredSize(new java.awt.Dimension(130, 40));
         btnModificar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnModificarActionPerformed(evt);
             }
         });
+        getContentPane().add(btnModificar, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 370, 150, -1));
 
         btnEliminar.setBackground(new java.awt.Color(14, 41, 84));
-        btnEliminar.setFont(new java.awt.Font("SF Pro Display", 1, 14)); // NOI18N
+        btnEliminar.setFont(new java.awt.Font("SF Pro Display", 1, 22)); // NOI18N
         btnEliminar.setForeground(new java.awt.Color(255, 255, 255));
         btnEliminar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/eliminar1.png"))); // NOI18N
         btnEliminar.setText("Eliminar");
+        btnEliminar.setPreferredSize(new java.awt.Dimension(130, 40));
         btnEliminar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnEliminarActionPerformed(evt);
             }
         });
+        getContentPane().add(btnEliminar, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 370, 140, -1));
 
         btnNuevo.setBackground(new java.awt.Color(14, 41, 84));
-        btnNuevo.setFont(new java.awt.Font("SF Pro Display", 1, 14)); // NOI18N
+        btnNuevo.setFont(new java.awt.Font("SF Pro Display", 1, 22)); // NOI18N
         btnNuevo.setForeground(new java.awt.Color(255, 255, 255));
         btnNuevo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/limpiar.png"))); // NOI18N
         btnNuevo.setText("Limpiar");
+        btnNuevo.setPreferredSize(new java.awt.Dimension(130, 40));
         btnNuevo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnNuevoActionPerformed(evt);
             }
         });
+        getContentPane().add(btnNuevo, new org.netbeans.lib.awtextra.AbsoluteConstraints(820, 370, 130, -1));
 
         jButton1.setBackground(new java.awt.Color(14, 41, 84));
-        jButton1.setFont(new java.awt.Font("SF Pro Display", 1, 14)); // NOI18N
+        jButton1.setFont(new java.awt.Font("SF Pro Display", 1, 22)); // NOI18N
         jButton1.setForeground(new java.awt.Color(255, 255, 255));
         jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/regresar.png"))); // NOI18N
         jButton1.setText("Regresar");
+        jButton1.setPreferredSize(new java.awt.Dimension(130, 40));
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(1020, 370, 150, -1));
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addGap(32, 32, 32)
-                .addComponent(btnAgregar, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(88, 88, 88)
-                .addComponent(btnModificar)
-                .addGap(89, 89, 89)
-                .addComponent(btnEliminar, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 94, Short.MAX_VALUE)
-                .addComponent(btnNuevo, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(94, 94, 94)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(38, 38, 38))
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnAgregar)
-                    .addComponent(btnModificar)
-                    .addComponent(btnEliminar)
-                    .addComponent(btnNuevo)
-                    .addComponent(jButton1))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-
-        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 360, 990, -1));
-
-        jPanel3.setBackground(new java.awt.Color(46, 138, 153));
-        jPanel3.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        jLabel6.setFont(new java.awt.Font("SF Pro Display", 1, 22)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel6.setText("BASE DE DATOS");
+        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 430, -1, -1));
 
         TablaRutas.setBackground(new java.awt.Color(137, 167, 161));
         TablaRutas.setFont(new java.awt.Font("SF Pro Display", 1, 13)); // NOI18N
@@ -268,34 +283,11 @@ public class IguRuta extends javax.swing.JFrame {
             TablaRutas.getColumnModel().getColumn(0).setPreferredWidth(20);
         }
 
-        jPanel3.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, 967, 230));
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 470, 967, 230));
 
-        jLabel6.setFont(new java.awt.Font("SF Pro Display", 1, 22)); // NOI18N
-        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel6.setText("BASE DE DATOS");
-        jPanel3.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 0, -1, -1));
-
-        getContentPane().add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 410, 990, -1));
-
-        jLabel4.setFont(new java.awt.Font("SF Pro Display", 1, 22)); // NOI18N
-        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel4.setText("DATOS");
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 80, -1, -1));
-
-        jPanel4.setBackground(new java.awt.Color(46, 138, 153));
-
-        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
-        jPanel4.setLayout(jPanel4Layout);
-        jPanel4Layout.setHorizontalGroup(
-            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1370, Short.MAX_VALUE)
-        );
-        jPanel4Layout.setVerticalGroup(
-            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 780, Short.MAX_VALUE)
-        );
-
-        getContentPane().add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1370, 780));
+        jLabel13.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/fondoOperadores.png"))); // NOI18N
+        jLabel13.setText("jLabel13");
+        getContentPane().add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1370, 770));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -365,6 +357,7 @@ public class IguRuta extends javax.swing.JFrame {
     private javax.swing.JButton btnNuevo;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel13;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
@@ -373,7 +366,6 @@ public class IguRuta extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JScrollPane jScrollPane1;
